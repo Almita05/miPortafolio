955374e45928fc61102eb415288c29f2c1baab0a
Almita05 <98787339+Almita05@users.noreply.github.com>
2023-10-07 13:52:50 -0600

    Add files via upload
    Añadí un archivo de JS para animaciones dentro de mi portafolio

---
+++ script.js
@@ -0,0 +1,39 @@
+// El DDM- Document object model modelo objeto del documento
+
+// Objeto en javascript 
+
+
+const nombre = "Alma"
+const edad = "21"
+
+const scoobyDoo = {
+    raza: "Gran Danés",
+    noPatas: 4,
+    puedeHablar: true,
+    color: "café con manchas"
+}
+
+// no funciona scoobyDoo = algo
+// esto si funciona
+//scoobyDoo = "café con manchas negras"
+
+// alert("Hola mundo")
+// console.log("Hola mundo")
+// document.write("Hola mundo ")
+
+//Animaciones
+let app = document.getElementById('app');
+
+let objetoDeLaBibliotecaTypewriter = new Typewriter(app, {
+    loop: true
+});
+
+objetoDeLaBibliotecaTypewriter.typeString('Alma Lilia')
+    .pauseFor(2500)
+    .deleteAll()
+    .typeString('¿Alma Lilia?')
+    .pauseFor(2500)
+    .deleteChars(12)
+    .typeString('<strong>¡Alma Lilia Martínez Landero!</strong>')
+    .pauseFor(2500)
+    .start();
